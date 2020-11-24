020304a2e13a37b1f09ba72aede098ba824ab775
billyjhayes8 <74617980+billyjhayes8@users.noreply.github.com>
2020-11-24 18:45:55 +0000

    Initial Basic Set Up
    Laid out starting area primitive objects, started on UI but only a little bit

---
+++ Total Apocolypse/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI healthText;
+
+    private int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+
+        UpdateHealth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateHealth()
+    {
+        int damage = 10;
+
+        health -= damage;
+
+        healthText.text = "Health: " + health;
+    }
+
+
+}
---
+++ Total Apocolypse/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 25.0f;
+    public float jummpForce = 2.5f;
+    public bool isOnGround = true;
+
+    private Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        float movementHorizontal = Input.GetAxis("Vertical");
+        float movementVertical = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(movementHorizontal, 0.0f, movementVertical);
+
+        playerRb.AddForce(movement * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jummpForce, ForceMode.Impulse);
+            isOnGround = false;
+            speed = 0.0f;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            speed = 50.0f;
+        }
+    }
+}
